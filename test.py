8db91ed80a80174d6b6e5fd65a0ef3f9e8bc46f0
HARRIXJANG <49017986+HARRIXJANG@users.noreply.github.com>
2022-07-20 15:01:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def load_h5_name_id(h5_filename):
 filenames_predict = [r'data\testdata.h5']
 
 # path to the best model weights
-model_save_weights_file = r'models\ASIN_best_weights.h5'
+model_save_weights_file = r'models\ASIN_weights1.h5'
 
 test_points_OnFace = None
 test_labels = None
